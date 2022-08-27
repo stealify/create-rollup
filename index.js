169b76aab562257437e65c0117089c2d8cd21536
Frank Lemanschik <frank@lemanschik.com>
2022-08-27 15:50:43 +0200

    Update index.js
    Now simply installs rollup and the most importent plugins as dev dependencies.

--- index.js
+++ index.js
@@ -1,84 +1,8 @@
 #!/usr/bin/env node
-const process = require('process');
-const path = require('path');
-const { basename, join } = require('path');
-const Debug = require('debug');
-const { 
-  writeFileSync, 
-  lstatSync,
-  readdirSync,
-  readFileSync,
-  existsSync,
-  mkdirSync
-} = require('fs');
+require('node:child_process').exec('npm i -D rollup @rollup/plugin-commonjs git+https://github.com/wessberg/rollup-plugin-ts', { stdio: "inhire"},
+(error, _stdout, _stderr) => (error) && console.error(`exec error: ${error}`))
 
-const { name } = JSON.parse(readFileSync('./package.json'));
-const debug = Debug('FileTransfer::');
-
-if (name === 'create-rollup' && process.cwd().indexOf('create-rollup') >-1 ) {
-  debug('Detected myself',process.cwd());
-  process.exit();
-}
-//if target is a directory a new file with the same name will be created
-const normalizeTarget = (target, source) => (existsSync(target) && lstatSync(target).isDirectory()) 
-  ? path.join(target, basename(source)) 
-  : target;
-
-const copyFileSync = (source, target) => 
- !existsSync(target) && writeFileSync(target, readFileSync(source));
-
-
-function copyRecursiveSync( source, target ) {
-  var files = [];
-
-  //check if folder needs to be created or integrated
-  let targetPath = (basename(source) === 'create-rollup') ? '' : basename(source);
-  if (targetPath === '') {
-    debug([basename( source )].join());  
-  }
-  var targetFolder = path.join( target, targetPath);
-    
-  if (basename( source ) === 'node_modules' || basename(source) === '.git') {
-    debug('targetFolder =>',targetFolder,basename( target));
-    debug('srcFolder =>',source,basename( target));
-    return;
-  }
-
-  debug('targetFolder =>',targetFolder);
-  if ( !existsSync( targetFolder ) ) {
-    mkdirSync( targetFolder );
-  }
-
-  //copy
-  if ( lstatSync(source).isDirectory() ) {
-    
-    readdirSync(source).forEach( ( file ) => {
-      var curSource = path.join( source, file ); 
-      if (basename(curSource) === '.git') {
-        return;
-      }
-
-      if (basename(curSource) === 'node_modules') {
-        return; 
-      }
-
-      debug('=>',curSource);
-      if ( fs.lstatSync( curSource ).isDirectory() ) {
-        copyRecursiveSync( curSource, targetFolder );
-      } else {
-        const normalizedTarget = normalizeTarget(targetFolder)
-        if (!existsSync(normalizedTarget)) {
-          copyFileSync( curSource, normalizedTarget );
-        } else {
-          console.warn(normalizedTarget, 'got created already please delete')
-        }
-      }
-    });
-  }
-}
-
-const src = __dirname;
-const dest = process.cwd();
-debug(`Copy: ${src} => ${dest}`);
-copyRecursiveSync(src,dest);
+// Sorry for the old behavior and code i guess this is much better it adds rollup to the dependencies.
+// npx create-rollup 
+// npm init rollup
 //https://github.com/stealify/create-rollup
